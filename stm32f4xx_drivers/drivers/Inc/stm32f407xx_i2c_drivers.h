44e9a8902100fe7f515fcef4a0b58fa2a346b9ae
Lalit Kumar <115681964+LalitK-Space@users.noreply.github.com>
2022-12-01 01:24:13 -0600

    Update stm32f407xx_i2c_drivers.h

--- stm32f4xx_drivers/drivers/Inc/stm32f407xx_i2c_drivers.h
+++ stm32f4xx_drivers/drivers/Inc/stm32f407xx_i2c_drivers.h
@@ -85,7 +85,7 @@ void I2C_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority);	// To confi
 // Other Helper APIs
 uint8_t I2C_getFlagStatus (I2C_RegDef_t *pI2Cx, uint32_t FlagName);     // To get Status Register Flags
 void I2C_PeripheralControl(I2C_RegDef_t *pI2Cx, uint8_t EnorDi);	// To enable or disable the I2C peripheral
-void I2C_ManageACK(I2C_RegDef_t *pI2Cx, uint8_t EnorDi);			// To enable or disable ACKing
+void I2C_ManageACK(I2C_RegDef_t *pI2Cx, uint8_t EnorDi);		// To enable or disable ACKing
 
 // Application Callbacks [To be implemented in the application]
 void I2C_ApplicationEventCallback(I2C_Handle_t *pI2CHandle, uint8_t ApplicationEvent);
