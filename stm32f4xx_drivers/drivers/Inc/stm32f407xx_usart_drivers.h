61fdc2805cfefcc68b11e375e9022515f1b552e2
Lalit Kumar <115681964+LalitK-Space@users.noreply.github.com>
2022-12-13 02:13:41 -0600

    Update stm32f407xx_usart_drivers.h

--- stm32f4xx_drivers/drivers/Inc/stm32f407xx_usart_drivers.h
+++ stm32f4xx_drivers/drivers/Inc/stm32f407xx_usart_drivers.h
@@ -41,9 +41,9 @@ typedef struct
 /* -- USART Configuration Macros -- */
 
 // USART Modes
-#define USART_MODE_TX_ONLY	0
-#define USART_MODE_RX_ONLY	1
-#define USART_MODE_TX_RX	2
+#define USART_MODE_TX_ONLY		0
+#define USART_MODE_RX_ONLY		1
+#define USART_MODE_TX_RX		2
 
 // USART Baud Rate
 #define USART_STD_BAUDRATE_1200		1200
@@ -60,10 +60,10 @@ typedef struct
 #define USART_STD_BAUDRATE_3M		3000000
 
 // USART Number of Stop Bits
-#define USART_STOPBITS_1			0
-#define USART_STOPBITS_0_5			1
-#define USART_STOPBITS_2			2
-#define USART_STOPBITS_1_5			3
+#define USART_STOPBITS_1		0
+#define USART_STOPBITS_0_5		1
+#define USART_STOPBITS_2		2
+#define USART_STOPBITS_1_5		3
 
 // USART Word Length
 #define USART_WORDLENGTH_8_BITS		0
@@ -72,12 +72,12 @@ typedef struct
 // USART Parity Control
 #define USART_PARITY_DISABLE		0
 #define USART_PARITY_EVEN_EN		1
-#define USART_PARITY_ODD_EN			2
+#define USART_PARITY_ODD_EN		2
 
 // USART Hardware Flow Control
-#define USART_HW_FLOW_CONTROL_NONE		0
-#define USART_HW_FLOW_CONTROL_CTS		1
-#define USART_HW_FLOW_CONTROL_RTS		2
+#define USART_HW_FLOW_CONTROL_NONE	0
+#define USART_HW_FLOW_CONTROL_CTS	1
+#define USART_HW_FLOW_CONTROL_RTS	2
 #define USART_HW_FLOW_CONTROL_CTS_RTS	3
 
 /* -- USART Status Flags (with Masking details) -- */
@@ -94,27 +94,27 @@ typedef struct
 
 
 /* -- Possible USART Application States (used in Data Tx and Rx APIs in Interrupt Mode) -- */
-#define USART_BUSY_READY	0
-#define USART_BUSY_IN_RX	1
-#define USART_BUSY_IN_TX	2
+#define USART_BUSY_READY		0
+#define USART_BUSY_IN_RX		1
+#define USART_BUSY_IN_TX		2
 
 /* -- Possible USART Application Events (Application callback) -- */
 // Event
 #define USART_EVENT_TX_COMPLETE		0
 #define USART_EVENT_RX_COMPLETE		1
-#define USART_EVENT_RX_IDLE			2
-#define USART_EVENT_RX_CTS			3
-#define USART_EVENT_RX_PE			4
+#define USART_EVENT_RX_IDLE		2
+#define USART_EVENT_RX_CTS		3
+#define USART_EVENT_RX_PE		4
 
 // Errors
-#define USART_ERROR_FE				5
-#define USART_ERROR_NE				6
-#define USART_ERROR_ORE				7
+#define USART_ERROR_FE			5
+#define USART_ERROR_NE			6
+#define USART_ERROR_ORE			7
 
 /* -- General MACROS -- */
 
 
-/* -- APIs Supported by SPI driver -- */
+/* -- APIs Supported by USART driver -- */
 
 // Peripheral Clock Setup
 // Enable/Disable Peripheral Clock for a given USART base address
@@ -135,7 +135,7 @@ uint8_t USART_ReceiveData_IT(USART_Handle_t *pUSARTHandle, uint8_t *pTxBuffer, u
 // IRQ Configuration and ISR Handling
 void USART_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi);     		// To configure IRQ number of the USART
 void USART_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority);		// To configure the priority
-void USART_IRQHandling(USART_Handle_t *pUSARTHandle);						// To handle the ISR
+void USART_IRQHandling(USART_Handle_t *pUSARTHandle);				// To handle the ISR
 
 // Other Helper APIs
 uint8_t USART_getFlagStatus (USART_RegDef_t *pUSARTx, uint32_t FlagName);     	// To get Status Register Flags
